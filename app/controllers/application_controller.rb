fea7442c53a61b26182c69ce8be585b7b933af51
Annemarie <a.lucernoni0@gmail.com>
2022-06-29 16:25:19 -0700

    commit tree 2d219ff14cfd5245c442bb07f034f166eb652598

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+    private
+
+    def invalid_record(exception)
+        # render json: {errors: "Can't be blank"}, status: :unprocessable_entity
+        #above works, below lets it be dynamic across models
+        render json: {errors: exception.record.errors}, status: :unprocessable_entity
+    end
 end
